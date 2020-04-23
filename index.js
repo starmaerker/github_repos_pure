280cc3261f456781cb67f0dd0ad0d9104ca5b189
Andreas Gropp <starmaerker@googlemail.com>
2020-04-23 19:01:27 +0200

    add image links

--- index.js
+++ index.js
@@ -11,14 +11,19 @@ const getRepos = async (lang) => {
       document.getElementById("inhalt").innerHTML = "";
   
       for(let i=0; i<30; i++) {
+          let avatar = todos.items[i].owner.avatar_url
+          let avatarLink = document.createElement("img")
+          avatarLink.setAttribute('src', avatar)          
           let child = document.createElement("p");
           let text = document.createTextNode(todos.items[i].name);
           let stargazers = document.createTextNode(todos.items[i].stargazers_count);
           let temp = document.createTextNode(": ");
+          
           child.appendChild(text);
           child.appendChild(temp);
-          child.appendChild(stargazers);
+          child.appendChild(stargazers);          
           inhalt.appendChild(child);
+          inhalt.appendChild(avatarLink)
       }     
   
       return todos;
